f22627b2da6984e39847964d986ca1b2a9c631e0
Rodrigo de Oliveira <rodrigooli1998@gmail.com>
2025-04-27 22:12:53 -0300

    chore: configure MainActivity and AndroidManifest to initialize application flow

--- app/src/main/java/com/rodrigooliveira/composesplashtemplate/MainActivity.kt
+++ app/src/main/java/com/rodrigooliveira/composesplashtemplate/MainActivity.kt
@@ -4,44 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.rodrigooliveira.composesplashtemplate.ui.theme.ComposeSplashTemplateTheme
+import com.rodrigooliveira.core.ui.theme.AppTheme
+import com.rodrigooliveira.feature.splash.presentation.SplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
-            ComposeSplashTemplateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            AppTheme {
+                SplashScreen(
+                    onNavigateToHome = {}
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeSplashTemplateTheme {
-        Greeting("Android")
     }
 }
